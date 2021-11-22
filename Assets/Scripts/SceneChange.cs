d3d107a9c56c6e7767a8f270e3270f44e3d9b86f
Stasioo <59345026+Stasioo@users.noreply.github.com>
2021-11-22 17:24:14 -0600

    Started working on menu system

---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    public void loadPlaysetVersion()
+    {
+        SceneManager.LoadScene("PlaysetVersion");
+    }
+
+    public void loadOriginalVersion()
+    {
+        SceneManager.LoadScene("ScareCoOffices");
+    }
+    public void loadGiantVersion()
+    {
+        Debug.Log("Loaded giant version scene");
+    }
+
+    public void loadCeilingVersion()
+    {
+        Debug.Log("Loaded ceiling version scene");
+    }
+}
