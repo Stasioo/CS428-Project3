53e1827508fedf3ecbb378b929e4a347a30f0c6f
Stasioo <59345026+Stasioo@users.noreply.github.com>
2021-11-23 13:27:43 -0600

    Started on prototype of rotation script

---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    public float speed = 50.0f;
+    private bool rotating = false;
+    private float rotateTime = 3.0f;
+    private float rotateDegrees = 90.0f;
+    private void Update()
+    {
+        if(Input.GetKeyDown("u"))
+        {
+            rotateObject();
+        }
+    }
+    public void rotateObject()
+    {
+        transform.Rotate(0,0,90);
+    }
+}
