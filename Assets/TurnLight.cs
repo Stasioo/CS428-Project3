a626ca7d9011846e0b93f154f18dfad0e46493c9
Stasioo <59345026+Stasioo@users.noreply.github.com>
2021-11-23 01:12:04 -0600

    Made lamp able to turn off and on, now need to make it interactable

---
+++ Assets/TurnLight.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnLight : MonoBehaviour
+{
+    public GameObject spotLight;
+    public GameObject lightObject;
+    public void turnLightOnOff()
+    {
+        if(spotLight.activeSelf)
+        {
+            spotLight.SetActive(false);
+            lightObject.SetActive(false);
+        } else
+        {
+            spotLight.SetActive(true);
+            lightObject.SetActive(true);
+        }
+    }
+}
